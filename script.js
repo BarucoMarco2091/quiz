57ac42a6cde067050b8d6d7223f19a8057fb4fcf
BarucoMarco2091 <108827736+BarucoMarco2091@users.noreply.github.com>
2024-10-01 15:52:17 -0300

    1

---
+++ script.js
@@ -0,0 +1,40 @@
+const quizData = [
+    {
+        question: "Qual o tipo de dado do valor 'true' em JavaScript?",
+        alternatives: ["String", "Boolean", "Number", "Undefined", "Object"],
+        correct: 1
+    },
+    {
+        question: "Qual método é usado para transformar uma string em maiúsculas?",
+        alternatives: [".split()", ".toUpperCase()", ".slice()", ".join()", ".reverse()"],
+        correct: 1
+    },
+    {
+        question: "Como você escreve um comentário em JavaScript?",
+        alternatives: ["<!-- comentário -->", "// comentário", "/* comentário */", "** comentário **", "# comentário"],
+        correct: 1
+    },
+    {
+        question: "Qual a função usada para exibir mensagens no console?",
+        alternatives: ["console.log()", "print()", "message()", "console.show()", "display()"],
+        correct: 0
+    },
+    {
+        question: "Como você cria uma função em JavaScript?",
+        alternatives: ["def minhaFuncao()", "function minhaFuncao()", "create minhaFuncao()", "fun minhaFuncao()", "newFunction()"],
+        correct: 1
+    }
+];
+
+let score = 0;
+let currentQuestion = 0;
+
+const startQuiz = document.querySelector('.start-button');
+startQuiz.addEventListener('click', function loadQuestion() {
+    const quizContainer = document.getElementById('quiz-container');
+    quizContainer.innerHTML = '';
+    const quizQuestions = document.createElement('div');
+    quizQuestions.className = 'quiz-question';
+    quizQuestions.innerHTML = quizData[currentQuestion].question;
+    quizContainer.appendChild(quizQuestions);
+});
